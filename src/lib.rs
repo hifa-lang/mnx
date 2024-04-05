fb061b14441913ec8d573f7eddf5b56e42cacbb1
kurotakazuki <kabisakanakabi@gmail.com>
2024-04-05 17:38:07 +0900

    feat: import mnx-schema.json

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![doc = include_str!("../README.md")]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod repositories;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use repositories::*;
---
+++ src/repositories/mod.rs
@@ -0,0 +1,4 @@
+use serde::{Deserialize, Serialize};
+use typify::import_types;
+
+import_types!("mnx/docs/mnx-schema.json");
